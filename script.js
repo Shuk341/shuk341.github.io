c7307e2d4bc4ea833cf14a9cd1e15d3449ca5d50
Shuk341 <118897251+Shuk341@users.noreply.github.com>
2026-03-30 14:16:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-// Typing Effect
-//let text = "Web Developer | WordPress | Shopify Expert";
-let text = "Web Developer | WordPress, Shopify & Frontend Specialist  <br> Building responsive, high-performing websites with a data-driven approach";
+// Typing
+let text = "Web Developer | WordPress | Shopify Specialist";
 let i = 0;
 
 function typing() {
@@ -12,8 +11,11 @@ function typing() {
 }
 typing();
 
-// Filter Projects
-function filterProjects(category) {
+// Tabs
+function setActive(button, category) {
+  document.querySelectorAll('.tabs button').forEach(btn => btn.classList.remove('active'));
+  button.classList.add('active');
+
   let cards = document.querySelectorAll('.project-card');
 
   cards.forEach(card => {
@@ -23,4 +25,31 @@ function filterProjects(category) {
       card.style.display = card.classList.contains(category) ? 'block' : 'none';
     }
   });
+}
+
+// Modal
+function openModal(type) {
+  let title = document.getElementById("modal-title");
+  let text = document.getElementById("modal-text");
+
+  if (type === "wp") {
+    title.innerText = "Business Website";
+    text.innerText = "Developed a responsive WordPress website using Elementor/Divi with focus on UI, performance and mobile optimization.";
+  }
+
+  if (type === "shopify") {
+    title.innerText = "E-commerce Store";
+    text.innerText = "Built Shopify store with product pages, cart system and optimized user experience.";
+  }
+
+  if (type === "html") {
+    title.innerText = "Landing Page";
+    text.innerText = "Created responsive landing page using HTML and CSS with clean UI.";
+  }
+
+  document.getElementById("modal").style.display = "flex";
+}
+
+function closeModal() {
+  document.getElementById("modal").style.display = "none";
 }
